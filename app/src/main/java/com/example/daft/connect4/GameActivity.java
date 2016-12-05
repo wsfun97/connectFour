3fcac459b056b1d9f5a10b0ecc6aa6f5f59098ef
wsfun97 <s1118171@ouhk.edu.hk>
2016-12-05 09:16:50 +0800

    Update GameActivity.java

--- app/src/main/java/com/example/daft/connect4/GameActivity.java
+++ app/src/main/java/com/example/daft/connect4/GameActivity.java
@@ -1,6 +1,7 @@
 package com.example.daft.connect4;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -130,5 +131,28 @@ public class GameActivity extends AppCompatActivity {
                 mediaPlayer.seekTo(0);
             }
             mediaPlayer.start();
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder quitDialog = new AlertDialog.Builder(GameActivity.this);
+        quitDialog.setTitle("Are you sure to quit the game?");
+
+        quitDialog.setPositiveButton("YES", new DialogInterface.OnClickListener(){
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // TODO Auto-generated method stub
+                finish();
+            }});
+
+        quitDialog.setNegativeButton("NO", new DialogInterface.OnClickListener(){
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                onResume();
+            }});
+
+        quitDialog.show();
     }
 }
